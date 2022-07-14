a354ff390b2abd1f8fa5240100e25e95a175869e
Igor Milenković <igor.milenkovic@localsearch.ch>
2022-07-14 15:04:54 +0200

    changes 3

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -154,8 +154,8 @@ const Index: Default<TemplateProps> = (props) => {
         <div className="section">
           <div className="grid grid-cols-3 gap-x-10 gap-y-10">
             <div className="bg-gray-100 p-5 space-y-12">
-              <h1 className="ls-headline">{headline}</h1>
-              <Contact address={address} phone={mainPhone}></Contact>
+              <h1 className="ls-headline">HD {headline}</h1>
+              {address && <Contact address={address} phone={mainPhone}></Contact>}
             </div>
             <div className="col-span-2 pt-5 space-y-10">
               <div>
