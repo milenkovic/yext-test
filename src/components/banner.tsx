2e56a1d428a69779f598c71f7eda50bbf0b6b905
Igor Milenković <igor.milenkovic@localsearch.ch>
2022-07-19 10:20:01 +0200

    Change of header

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -34,6 +34,7 @@ const Banner = (props: Banner) => {
     <>
       <div className="bg-red-900 text-5xl font-bold text-white p-10 flex items-center justify-center flex-row space-x-20 w-full">
         <div className="flex-col space-y-10 text-center">
+          <div>Contact us</div>
           {/* <div>{name}</div>
           <div>{renderPrettyAddress(address)}</div>
           <div>Open Until {openTime}</div> */}
